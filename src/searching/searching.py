81becba577a877039093429ced47c76ef15c2dca
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-08-18 11:41:19 -0700

    adding commited fiels

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,17 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    if end is None:
+        end = len(arr) - 1
+    if start > end:
+        return end
+    
+    mid = (start + end) // 2
+    if target == arr[mid]:
+        return mid
+    if target < arr[mid]:
+        return binary_search(arr, target, start, mid - 1)
+    return binary_search(arr, target, mid + 1, end)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +22,16 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    start = 0
+    end = len(arr) - 1
 
+    while start < end:
+        mid = (start + end) // 2
+        if target == arr[mid]:
+            return target
+        elif target < mid:
+            end = mid - 1
+        else:
+            start = mid + 1
+        
+    return False
